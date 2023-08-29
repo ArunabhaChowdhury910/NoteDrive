352456a117ae19dc86c28a6c9ff59f9676c770c4
Arunabha <mailtoarunabha1234.e@google.com>
2023-08-29 11:02:35 +0530

    One bug frixed not responsive yet and some bugs left

--- NoteDrive/components/navbar/uppernav.vue
+++ NoteDrive/components/navbar/uppernav.vue
@@ -1,21 +1,31 @@
 <template>
-    <div class="flex  bg-main_heading_background">
-        <NuxtLink to="/" class="text-3xl max-md:h-3 text-white font-Inter font-normal p-5">NOTEDRIVE</NuxtLink>  
-        <div class="flex justify-end items-center w-full gap-12 max-md:hidden">
-            
-            <NuxtLink to="/" class="text-xl text-white font-Inter font-normal p-5 hover:text-dark_main_color">About Us</NuxtLink>
-            <NuxtLink to="/" class="text-xl text-white font-Inter font-normal p-5 hover:text-dark_main_color">Join Us</NuxtLink>
-            <NuxtLink to="/" class="text-xl text-white font-Inter font-normal p-5 hover:text-dark_main_color mr-3">Login</NuxtLink>
+    <div class="p-1 bg-main_heading_background">
+        <div class="flex">
+            <NuxtLink to="/" class="text-3xl max-md:h-3 text-white font-Inter font-normal p-5">NOTEDRIVE</NuxtLink>  
+            <div class="md:hidden flex justify-end w-full px-5">
+                <Icon name="prime:bars" class=" text-white cursor-pointer my-3 items-end text-5xl" @click="toggleMenu"/>
+            </div>
+            <div class="flex justify-end items-center w-full gap-12 max-md:hidden">
+                
+                <NuxtLink to="/" class="text-xl text-white font-Inter font-normal p-5 hover:text-dark_main_color">About Us</NuxtLink>
+                <NuxtLink to="/" class="text-xl text-white font-Inter font-normal p-5 hover:text-dark_main_color">Join Us</NuxtLink>
+                <NuxtLink to="/" class="text-xl text-white font-Inter font-normal p-5 hover:text-dark_main_color mr-3">Login</NuxtLink>
+            </div>
         </div>
         <div class="md:hidden w-full left-1.5">
-            <Icon name="prime:bars" class="flex ml-321 justify-end cursor-pointer my-3 w-full items-end text-5xl" @click="toggleMenu"/>
-            <div v-if="isMenuOpen" class="  w-full justify-start items-start bg-main_heading_background mt-2 p-2">
-                <NuxtLink to="/" class="block text-xl text-white font-Inter font-normal p-2 hover:text-dark_main_color">
-                About Us</NuxtLink>
-                <NuxtLink to="/" class="block text-xl  text-white font-Inter font-normal p-2 hover:text-dark_main_color">
-                Join Us</NuxtLink>
-                <NuxtLink to="/" class="block text-xl w-full text-left text-white font-Inter font-normal p-2 hover:text-dark_main_color"
-                >Login</NuxtLink>
+            <!-- <div class="flex justify-end w-full px-5">
+                <Icon name="prime:bars" class="  cursor-pointer my-3 items-end text-5xl" @click="toggleMenu"/>
+            </div> -->
+
+            <div v-if="isMenuOpen" class=" w-full flex-col justify-start items-start bg-main_heading_background mt-2 p-2">
+                <div class="flex-col justify-start w-full">
+                    <NuxtLink to="/" class="block text-xl text-white font-Inter font-normal p-2 hover:text-dark_main_color">
+                    About Us</NuxtLink>
+                    <NuxtLink to="/" class="block text-xl  text-white font-Inter font-normal p-2 hover:text-dark_main_color">
+                    Join Us</NuxtLink>
+                    <NuxtLink to="/" class="block text-xl text-left text-white font-Inter font-normal p-2 hover:text-dark_main_color"
+                    >Login</NuxtLink>
+                </div>
             </div>
 </div>
 
